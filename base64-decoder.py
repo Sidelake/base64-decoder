4387c800854e730f87b782964214928b95b59dcc
jin <82104987+Sidelake@users.noreply.github.com>
2021-05-25 15:31:30 +0200

    Update base64-decoder.py

--- base64-decoder.py
+++ base64-decoder.py
@@ -1,9 +1,13 @@
 import base64
-
-text = 'insert random base64 text'
+#imports
+print('========================')
+text = 'aW5zZXJ0IHJhbmRvbSBiYXNlNjQgdGV4dA=='
 kekd = text.encode('ascii')
 text = base64.b64decode(kekd)
 final = text.decode('ascii')
 print(final)
 
-e = input('Press any key to exit!')
+e = input("""=========================
+Press any key to exit!""")
+
+#note: this was extremely easy to make lol.
