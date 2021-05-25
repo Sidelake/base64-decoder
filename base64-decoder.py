d3b413eb6eedf31120c5500ff9d182ccffdd00cf
jin <82104987+Sidelake@users.noreply.github.com>
2021-05-25 15:20:12 +0200

    Update base64-decoder.py

--- base64-decoder.py
+++ base64-decoder.py
@@ -1,7 +1,9 @@
 import base64
+import os
 
 text = 'insert random base64 text'
 kekd = text.encode('ascii')
 text = base64.b64decode(kekd)
 final = text.decode('ascii')
 print(final)
+input('Press any key to exit!')
